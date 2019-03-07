37b64363705ed296082a4a4fbe82e2d13c4bca25
Azizbek Gaybullaev <goazizbek@gmail.com>
2019-03-07 21:11:46 +0500

    first commit

---
+++ content/how-to-write-ssg.md
@@ -0,0 +1,10 @@
+title: How to become a senior software developer?
+date: 2019-03-10
+slug: how-to-become-senior-software-developer
+summary: You have been a junior or a middle developer for too long now. You feel like you deserve more. You feel like it is time to grab life by the balls and rise up the career ladder.
+
+You have been a junior or a middle developer for too long now. You feel like you deserve more. You feel like it is time
+to grab life by the balls and rise up the career ladder. Well, congratulations! You are so lucky to be reading this blog post
+because I am about to reveal a secret ingredient I used to become a senior software developer. This is a unique ingredient
+which no one else is aware of and which will skyrocket your career in no time. But I warn you! This is not an easy feat
+to accomplish. Ready? Let's go.
---
+++ task.py
@@ -0,0 +1,116 @@
+import os
+from datetime import datetime
+
+from markdown2 import markdown as md
+from jinja2 import Environment, FileSystemLoader, PackageLoader
+
+
+
+
+BASE_DIR = os.getcwd()
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+CONTENT_DIR = os.path.join(BASE_DIR, 'content')
+OUTPUT_DIR = os.path.join(BASE_DIR, 'output')
+
+def get_md_posts():
+    for entry in os.scandir(CONTENT_DIR):
+        if all([
+            not entry.name.startswith('.'),
+            entry.name.endswith('.md'),
+            entry.is_file()
+        ]):
+            yield entry.name
+
+
+def generate_html(files):
+    posts = {}
+
+    for file in files:
+        with open(os.path.join(CONTENT_DIR, file)) as f:
+            posts[file] = md(f.read(), extras=['metadata'])
+
+    posts = {
+        post: posts[post] for post in sorted(posts, key=lambda post: datetime.strptime(posts[post].metadata['date'], '%Y-%m-%d'), reverse=True)
+    }
+
+    env = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
+    home_template = env.get_template('home.html')
+    post_template = env.get_template('post-detail.html')
+
+    index_posts_metadata = [posts[post].metadata for post in posts]
+
+    index_html_content = home_template.render(posts=index_posts_metadata)
+
+
+    with open(os.path.join(OUTPUT_DIR, 'home.html'), 'w') as f:
+        f.write(index_html_content)
+
+    # print(index_html_content)
+
+    for post in posts:
+        post_metadata = posts[post].metadata
+
+        post_data = {
+            'content': posts[post],
+            'title': post_metadata['title'],
+            'date': post_metadata['date'],
+        }
+
+        post_html_content = post_template.render(post=post_data)
+
+        post_file_path = 'output/posts/{slug}/home.html'.format(slug=post_metadata['slug'])
+
+        os.makedirs(os.path.dirname(post_file_path), exist_ok=True)
+        with open(post_file_path, 'w') as file:
+            file.write(post_html_content)
+
+
+
+def main():
+    if not os.path.exists(OUTPUT_DIR):
+        os.mkdir(OUTPUT_DIR)
+
+    generate_html(get_md_posts())
+
+# POSTS = {}
+# for markdown_post in os.listdir('content'):
+#     file_path = os.path.join('content', markdown_post)
+#
+#     with open(file_path, 'r') as file:
+#         POSTS[markdown_post] = md(file.read(), extras=['metadata'])
+#
+# POSTS = {
+#     post: POSTS[post] for post in sorted(POSTS, key=lambda post: datetime.strptime(POSTS[post].metadata['date'], '%Y-%m-%d'), reverse=True)
+# }
+#
+#
+# env = Environment(loader=PackageLoader('task', 'templates'))
+# index_template = env.get_template('home.html')
+# post_template = env.get_template('post-detail.html')
+#
+# index_posts_metadata = [POSTS[post].metadata for post in POSTS]
+#
+# index_html_content = index_template.render(posts=index_posts_metadata)
+#
+# with open('output/home.html', 'w') as file:
+#     file.write(index_html_content)
+#
+# for post in POSTS:
+#     post_metadata = POSTS[post].metadata
+#
+#     post_data = {
+#         'content': POSTS[post],
+#         'title': post_metadata['title'],
+#         'date': post_metadata['date'],
+#     }
+#
+#     post_html_content = post_template.render(post=post_data)
+#
+#     post_file_path = 'output/posts/{slug}/index.html'.format(slug=post_metadata['slug'])
+#
+#     os.makedirs(os.path.dirname(post_file_path), exist_ok=True)
+#     with open(post_file_path, 'w') as file:
+#         file.write(post_html_content)
+
+if __name__ == '__main__':
+    main()
